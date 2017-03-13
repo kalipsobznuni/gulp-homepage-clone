c9906e0f110a885a714f9dee6d237fc8f38d8cfe
kalipsobznuni <kalipso.bznuni14159@gmail.com>
2017-03-14 03:25:40 +0400

    add basic css layout

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
 
                               </h2>
                               <div className='ctas'>
-                                <a class='ctas-button' href ='https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md'>
+                                <a className='ctas-button' href ='https://github.com/gulpjs/gulp/blob/master/docs/getting-started.md'>
                                 Get Started
                                 </a>
                               </div>
@@ -61,73 +61,73 @@ class App extends Component {
                       </div>
                     </header>
 
-                    <div class="feature3">
-                     <div class="container-lrg flex baseline">
-                       <div class="col-4">
-                         <b class="svg center"><img class="mask-img" src="images/browser.svg" /> </b>
-                         <h3 class="subheading center">Simple Usage</h3>
-                         <p class="paragraph">
+                    <div className="feature3">
+                     <div className="container-lrg flex baseline">
+                       <div className="col-4">
+                         <b className="svg center"><img class="mask-img" src="images/browser.svg" /> </b>
+                         <h3 className="subheading center">Simple Usage</h3>
+                         <p className="paragraph">
                            By preferring code over configuration, node best practices, and a minimal API surface - gulp makes things simple like never before.
                          </p>
                        </div>
-                       <div class="col-4">
-                         <b class="svg center"><img class="mask-img" src="images/guage.svg"/></b>
-                         <h3 class="subheading center">Efficient Builds</h3>
-                         <p class="paragraph">
+                       <div className="col-4">
+                         <b className="svg center"><img class="mask-img" src="images/guage.svg"/></b>
+                         <h3 className="subheading center">Efficient Builds</h3>
+                         <p className="paragraph">
                            Using the power of node streams, gulp gives you fast builds that don't write intermediary files to disk.
                          </p>
                        </div>
-                       <div class="col-4">
-                         <b class="svg center"><img class="mask-img" src="images/atom.svg"/></b>
-                         <h3 class="subheading center">Quality Ecosystem</h3>
-                         <p class="paragraph">
+                       <div className="col-4">
+                         <b className="svg center"><img class="mask-img" src="images/atom.svg"/></b>
+                         <h3 className="subheading center">Quality Ecosystem</h3>
+                         <p className="paragraph">
                            By enforcing strict guidelines, we ensure our plugins stay simple and work as expected.
                          </p>
                        </div>
                      </div>
                    </div>
 
-                   <div class="whatisgulp">
-                      <div class="container-lrg">
-                        <div class="col-12">
-                          <div class="editor-window">
-                            <div class="editor-menubar">
-                              <span class="editor-button close"></span>
-                              <span class="editor-button minimize"></span>
-                              <span class="editor-button maximize"></span>
-                              <span class="editor-filename">gulpfile.js</span>
+                   <div className="whatisgulp">
+                      <div className="container-lrg">
+                        <div className="col-12">
+                          <div className="editor-window">
+                            <div className="editor-menubar">
+                              <span className="editor-close"></span>
+                              <span className="editor-minimize"></span>
+                              <span className="editor-maximize"></span>
+                              <span className="editor-filename">gulpfile.js</span>
                             </div>
-                            <span class="hljs-keyword">var</span> gulp = <span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp'</span>);
-                            <span class="hljs-keyword">var</span> pug = <span class="mobile-show"><br />  </span><span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp-pug'</span>);
-                            <span class="hljs-keyword">var</span> less = <span class="mobile-show"><br />  </span><span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp-less'</span>);
-                            <span class="hljs-keyword">var</span> minifyCSS = <span class="mobile-show"><br />  </span><span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp-csso'</span>);
+                            <span className="hljs-keyword">var</span> gulp = <span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp'</span>);
+                            <span className="hljs-keyword">var</span> pug = <span class="mobile-show"><br />  </span><span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp-pug'</span>);
+                            <span className="hljs-keyword">var</span> less = <span class="mobile-show"><br />  </span><span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp-less'</span>);
+                            <span className="hljs-keyword">var</span> minifyCSS = <span class="mobile-show"><br />  </span><span class="hljs-built_in">require</span>(<span class="hljs-string">'gulp-csso'</span>);
                             some code here
                           </div>
                         </div>
                       </div>
                     </div>
 
-                  <div class="socialproof">
-                            <div class="container-sml">
-                              <div class="flex text-center">
-                                <div class="col-12">
-                                  <h4 class="subheading">
+                  <div className="socialproof">
+                            <div className="container-sml">
+                              <div className="flex text-center">
+                                <div className="col-12">
+                                  <h4 className="subheading">
                                     "Builds can be the most awful sinkhole for teams to waste their time with - gulp is a serious win for any project."
                                   </h4>
-                                  <p class="paragraph">Eric, Co-founder @ stae</p>
+                                  <p className="paragraph">Eric, Co-founder @ stae</p>
                                 </div>
                               </div>
                             </div>
                           </div>
-                          <div class="footer">
-                            <div class="container-sml">
-                              <div class="col-12 text-center">
-                                <div class="contribute-copy center vertical text-center">
-                                  <h3 class="subheading">Become a backer </h3>
-                                  <p class="paragraph">
+                          <div className="footer">
+                            <div className="container-sml">
+                              <div className="col-12 text-center">
+                                <div className="contribute-copy center vertical text-center">
+                                  <h3 className="subheading">Become a backer </h3>
+                                  <p className="paragraph">
                                     Support the community and keep development going strong.
                                   </p>
-                                  <a class="ctas-button" href="https://opencollective.com/gulpjs">Donate</a>
+                                  <a className="ctas-button" href="https://opencollective.com/gulpjs">Donate</a>
                                 </div>
                               </div>
                             </div>
